5be5fcc11ae1d03ba245bfdd723cee7e1141e31f
pankajverma-1 <pankajverma1361861@gmail.com>
2022-12-25 01:57:18 +0530

    diploy3

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 require('./db/conn');
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./models/Router');
+const dirname = path.resolve();
 const port = process.env.PORT || 500;
 
 const app = express();
@@ -14,5 +16,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+app.use(express.static(path.join(dirname, '/client/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(dirname, '/client/build/index.html'))
+);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
 
 app.listen(port, () => console.log(`server ${port}`));
